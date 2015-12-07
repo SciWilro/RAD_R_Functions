3aa1415afe48d3fad2aeaf1279a5745681d20f17
bwringe <bwringe@mun.ca>
2015-12-07 09:59:38 -0330

    Added functionality that will take your results out the NH folder, and add them to a nice results folder

--- NewHybridsAutomateProj.R
+++ NewHybridsAutomateProj.R
@@ -78,4 +78,38 @@ NH.auto.OSX <- function(folder.data, where.NH, burnin, sweeps){
         mclapply(X = jobs.vector, FUN = system, mc.cores = h.cores)
     }
     
+    
+    ## create a new folder to put the results in - remember the inception thing - be smart
+    dir.create(path = paste0(folder.data, "NH.Results"))
+    
+    res.path.make <- paste0(folder.data, "NH.Results") ## get the path to the new results folder
+    
+    # k = 1 # for internal code checking
+    
+    ## get the results generated for each copy of NH/file to be analyzed
+    for(k in 1:length(NH.copy.list)){
+        
+        ## make a nice name for the folders of resutls we are goign to make - so so pretty
+        res.name <- paste0(files.anal[k], "_Results") # file name + results - not as pretty as it could be
+        dir.create(path = paste0(res.path.make, "/", res.name)) ## make that folder to put the resutls in
+        
+        where.the.results.to <- paste0(res.path.make, "/", res.name, "/") ## where did you make that folder?
+        
+        NH.copy.to.get <- NH.copy.list[k] ## what copy of NH are we goign to look in?
+        find.res.vec <- list.files(path = paste0(where.temp, NH.copy.to.get), pattern = "aa-") ## find all the files that
+        ## begin with "aa-" inside that copy of NH - big shout out to Eric Anderson for using a regular string in the results name!
+        
+        find.res.vec.path <- paste0(paste0(where.temp, NH.copy.to.get, "/"), find.res.vec) ## find the path to all the files ID'd
+        file.copy(from = find.res.vec.path, to = where.the.results.to) ## copy those files to the proper results folder
+        
+        ## keep making it pretty by renaming the results files so you know what data set they are associated with
+        rename.vec <- gsub(x = find.res.vec, pattern = "aa-", replacement = paste0(files.anal[k], "_"))
+        
+        ## again - need a vectr that shows their computr postion
+        rename.vec.path <- paste0(where.the.results.to, rename.vec) ## vector with new names
+        old.name.vec.path <- paste0(where.the.results.to, find.res.vec) ## vector with old names
+        
+        file.rename(from = old.name.vec.path, to = rename.vec.path) ## check that vectorized functionality in file.rename - mad props!
+        
+    
 }
