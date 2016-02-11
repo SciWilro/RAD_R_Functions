4bebeb2b7eb2fab67484de95bd70ad0f1579299b
NickJeff13 <nick.w.jeffery3@gmail.com>
2016-02-11 16:27:54 -0400

    NewHybrids Z Score Script
    Adds z scores from a csv file into a modified NewHybrids input file,
    merges them together and writes a new input file.

---
+++ NewHybs_AddZScore.R
@@ -0,0 +1,35 @@
+#Add a column to a NH input file to include the known genotype classes (e.g. z1, etc)
+library(dplyr)
+library(tidyr)
+setwd("C:/Users/Nick/Desktop")
+#Read in the file
+
+NHdata <- read.table("Corr_West.top96_wild.unlinked.S1_R2NH.txt", header = FALSE, sep = "\t", quote = "", stringsAsFactors = FALSE)
+head(NHdata[1:5,])
+#Save the first 5 rows to be added back in at the end.
+addbackin<-NHdata[1:5,]
+
+#Now read it back in but without the first 5 rows.
+NHdata2 <- read.table("Corr_West.top96_wild.unlinked.S1_R2NH.txt", header = FALSE, sep = "\t", quote = "", stringsAsFactors = FALSE,skip=5)
+head(NHdata2)
+#Give the column a name for the separate function to work
+names(NHdata2)<-"Data2"
+Nhdata3<-separate(data = NHdata2,col=Data2,into = c("Individual",rep("SNP",96)),sep = " ")
+
+Nhdata4<-as.data.frame(append(Nhdata3,list(Zcode=NA), after = 1))
+write.csv(x = Nhdata4,file = "NHdatasplit.csv",row.names = FALSE, col.names = TRUE)
+
+###Next, want to make a csv of your z codes per individual that is separate from the file with all the locus names
+#Then merge these together
+Nhdata5<-read.csv("NHdatasplit2.csv")
+Zscorevector<-read.csv("ZvectorforNH.csv")
+#Merge
+NHfinal<- merge(y=Nhdata5,x=Zscorevector,by="Individual",all=TRUE)
+head(NHfinal)
+
+#Now make this an actual file readable by NewHybrids
+Loci <- do.call(paste,c(NHfinal[,], sep=" "))
+Loci<-gsub(pattern = "NA"," ",x = Loci)
+Loci2<-c(addbackin,Loci)
+
+write.table(Loci2,file = "NHFinalInput.txt",col.names=FALSE,row.names=FALSE,quote=FALSE)
